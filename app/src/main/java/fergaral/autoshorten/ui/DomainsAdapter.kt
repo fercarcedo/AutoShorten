51a9285539fb08328348f685d090bffb1c4337f9
Fernando Garcia Alvarez <fercarcedo@gmail.com>
2018-09-02 13:14:37 +0200

    Target Android Pie

--- app/src/main/java/fergaral/autoshorten/ui/DomainsAdapter.kt
+++ app/src/main/java/fergaral/autoshorten/ui/DomainsAdapter.kt
@@ -4,8 +4,6 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.TextView
 import fergaral.autoshorten.R
 import fergaral.autoshorten.data.Domain
 import kotlinx.android.synthetic.main.domain_item.view.*
@@ -15,14 +13,14 @@ import kotlinx.android.synthetic.main.domain_item.view.*
  */
 class DomainsAdapter(private var items: List<Domain>, private val onRemoveListener: (Domain) -> Unit): RecyclerView.Adapter<DomainsAdapter.DomainViewHolder>() {
 
-    override fun onBindViewHolder(holder: DomainViewHolder?, position: Int) {
-        holder?.bind(items[position], onRemoveListener)
+    override fun onBindViewHolder(holder: DomainViewHolder, position: Int) {
+        holder.bind(items[position], onRemoveListener)
     }
 
     override fun getItemCount(): Int = items.count()
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): DomainViewHolder {
-        val view = LayoutInflater.from(parent?.context).inflate(R.layout.domain_item, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DomainViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.domain_item, parent, false)
         return DomainViewHolder(view, items)
     }
 
@@ -34,13 +32,8 @@ class DomainsAdapter(private var items: List<Domain>, private val onRemoveListen
     }
 
     class DomainViewHolder(view: View, private val items: List<Domain>): RecyclerView.ViewHolder(view) {
-        val btnDelete: ImageButton
-        val tvDomain: TextView
-
-        init {
-            btnDelete = view.btnDeleteDomain
-            tvDomain = view.tvDomain
-        }
+        private val btnDelete = view.btnDeleteDomain
+        private val tvDomain = view.tvDomain
 
         fun bind(domain: Domain, onRemoveListener: (Domain) -> Unit) {
             tvDomain.text = domain.url
