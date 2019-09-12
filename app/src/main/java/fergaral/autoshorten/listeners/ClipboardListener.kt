6c47387251f0be0efb2faa3b45a35a7b752e5c03
Fernando Garcia Alvarez <fercarcedo@gmail.com>
2019-09-12 02:29:57 +0200

    Updated dependencies

--- app/src/main/java/fergaral/autoshorten/listeners/ClipboardListener.kt
+++ app/src/main/java/fergaral/autoshorten/listeners/ClipboardListener.kt
@@ -9,7 +9,6 @@ import fergaral.autoshorten.util.copyText
 import fergaral.autoshorten.util.getClipboardText
 import fergaral.autoshorten.util.isURL
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
 import javax.inject.Inject
 
 /**
